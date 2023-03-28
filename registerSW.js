1b40abacacfcecf1690750e725990c50b32c32fe
vysahjk <vysahjk@gmail.com>
2023-03-28 16:35:30 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mobile_workbox_tp/sw.js', { scope: '/mobile_workbox_tp/' })})}
