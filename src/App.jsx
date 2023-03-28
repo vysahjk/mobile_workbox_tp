44181fe3792d897a3cff783473687af836eea213
vysahjk <vysahjk@gmail.com>
2023-03-28 16:31:13 +0200

    chore: add list fonctionnalites

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-
+  const [browserName, setBrowserName] = useState("")
   const [supportsPWA, setSupportsPWA] = useState(false);
   const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+    setBrowserName(navigator.userAgent)
+  }, [])
 
   useEffect(() => {
     const handler = e => {
@@ -16,22 +18,6 @@ function App() {
     window.addEventListener("beforeinstallprompt", handler);
   }, [])
 
-  const iOS = () => {
-    return (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-  }
-
-  const requestNotification = () => {
-    if (!iOS()) {
-      Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
-          
-        }
-      })
-    } else {
-      
-    }
-  }
-
   const installApp = evt => {
     evt.preventDefault();
     if (!promptInstall) {
@@ -39,6 +25,7 @@ function App() {
     }
     promptInstall.prompt();
   };
+
 
   return (
     <div className="App">
@@ -48,7 +35,7 @@ function App() {
         <p>Les PWA sont une alternative aux application oneline performante que l'on peut installer sur nos pc et smartphones</p>
       </div>
 
-      <h2>Pros:</h2>
+      <h2>Avantages:</h2>
       <ul>
         <li>Ils peuvent fonctionner hors ligne !</li>
         <li>Moins d'espace dans votre disque de stockage et de mémoire</li>
@@ -57,9 +44,45 @@ function App() {
         <li>Elle peuvent envoyer de notifcations</li>
       </ul>
 
+      
 
-      {supportsPWA ? 
-        <button onClick={installApp}>install app</button> :
+      <h3>Navigateur necessaire:</h3>
+      <ul>
+        <li>Chrome</li>
+        <li>Edge</li>
+        <li>Safari</li>
+        <li>Firefox</li>
+      </ul>
+
+      <h3 style={{color: "salmon"}}>Votre navigateur actuel: {browserName}</h3>
+
+      <h3>Fonctionnalités disponible</h3>
+
+      <ul>
+        <li>Service worker</li>
+        <li>Background Sync</li>
+        <li>Push notifications</li>
+        <li>Cache storage (offline)</li>
+        <li>Payments</li>
+        <li>Geolocation</li>
+        <li>Fullscreen</li>
+        <li>Copy & paste Text, PNG</li>
+        <li>Contacts picker</li>
+      </ul>
+
+      <p>Les fonctionnalités disponibles restent dépendantes à la fois du navigateur et de l’OS</p>
+
+      <h3 style={{color: "salmon"}}>Votre os: {navigator.platform}</h3>
+
+      {!supportsPWA ? 
+      <>
+        <p>Vous pouvez installer PWA dès maintenat</p>
+        <button style={{
+           backgroundColor: 'green',
+           color: "white"}}
+         onClick={installApp}>installer PWA</button>
+      </>
+       :
       <div
         style={{ color: "red"}}
       >Le PWA n'est pas supporté actuelement</div>}
